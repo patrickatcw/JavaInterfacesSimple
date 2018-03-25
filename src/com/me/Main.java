897fd36ffbc1a2648a8a5457fb117576c15b4d53
patrickatcw <pj8392@yahoo.com>
2018-03-25 11:49:47 -0400

    set up initial structure and updated gitignore

---
+++ src/com/me/Main.java
@@ -0,0 +1,11 @@
+package com.me;
+/*
+interface -
+a bit like a class, except a Java interface can only contain method
+signatures and fields. An Java interface cannot contain an implementation
+of the methods, only the signature (name, parameters and exceptions) of
+the method
+ */
+
+public class Main {
+}
